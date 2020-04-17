1257e01555f394ba97c8a04fd5e16904bea0ce92
LuddeOnGit <hdguys11@gmail.com>
2020-04-17 20:38:08 +0200

    Added suport for lower characters

--- 1-to-1.py
+++ 1-to-1.py
@@ -7,10 +7,13 @@ Created on Fri Apr 17 13:58:41 2020
 """
 
 from termcolor import colored
+import string
 
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#problem = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 #replacements = {"A":None, "B":None, "C":None, "D":None, "E":None, "F":None, "G":None, "H":None, "I":None, "J":None, "K":None, "L":None, "M":None, "N":None, "O":None, "P":None, "Q":None, "R":None, "S":None, "T":None, "U":None, "V":None, "W":None, "X":None, "Y":None, "Z":None}
 replacements = {}
+for l in string.ascii_uppercase:
+    replacements[l] = "|"
 
 text = ""
 
@@ -18,18 +21,19 @@ def printText():
     # Prints the text with the given replacements
     
     for character in text:
-        if character in replacements:
+        if character in replacements and replacements[character] != "|":
             print(replacements[character], end="")
-        elif character in alphabet:
+        elif character in string.ascii_uppercase:
             print(colored(character, "yellow"), end="")
         else:
             print(character, end="")
 
 def printReps():
-    print(alphabet)
-    for c in alphabet:
-        if c in replacements:
-            print(replacements[c], end="")
+    print(string.ascii_uppercase)
+    sorted_replacements = {v: k for k, v in replacements.items()}
+    for k,repl in sorted_replacements.items():
+        if repl != "|":
+            print(repl, end="")
         else:
             print("-", end="")
 
@@ -37,12 +41,12 @@ def printReps():
     
     
 
-text = input("Paste text: ")
+text = input("Paste text: ").upper()
 
 
 while True:
     
-    ass = input("New assumption: ")
+    ass = input("New assumption: ").upper()
     keys, values = ass.split("=")
     if len(keys) != len(values): 
         if values[-1] == "!":
@@ -51,12 +55,12 @@ while True:
         else:
             continue
     for i in range(len(keys)):
-        if keys[i] in replacements:
-            if replacements[keys[i]] != values[i]:
+        if keys[i] in string.ascii_uppercase:
+            if replacements[keys[i]] != values[i] and replacements[keys[i]] != "|":
                 print("CONFLICT FOUND with chatacter " + keys[i])
                 continue
             
-        replacements[keys[i]] = values[i]
+            replacements[keys[i]] = values[i]
         
     printReps()
     printText()
