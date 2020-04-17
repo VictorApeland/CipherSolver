23c9dc4565dd92c49ab7449796f8d7b07eea874f
LuddeOnGit <hdguys11@gmail.com>
2020-04-17 21:43:29 +0200

    Added letter frequency chart and made small change to 1-to-1

--- 1-to-1.py
+++ 1-to-1.py
@@ -1,19 +1,8 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 17 13:58:41 2020
-
-@author: victorapeland
-"""
-
 from termcolor import colored
-import string
+from string import ascii_uppercase as alphabet
 
-#problem = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 #replacements = {"A":None, "B":None, "C":None, "D":None, "E":None, "F":None, "G":None, "H":None, "I":None, "J":None, "K":None, "L":None, "M":None, "N":None, "O":None, "P":None, "Q":None, "R":None, "S":None, "T":None, "U":None, "V":None, "W":None, "X":None, "Y":None, "Z":None}
 replacements = {}
-for l in string.ascii_uppercase:
-    replacements[l] = "|"
 
 text = ""
 
@@ -21,19 +10,19 @@ def printText():
     # Prints the text with the given replacements
     
     for character in text:
-        if character in replacements and replacements[character] != "|":
+        if character in replacements:
             print(replacements[character], end="")
-        elif character in string.ascii_uppercase:
+        elif character in alphabet:
             print(colored(character, "yellow"), end="")
         else:
             print(character, end="")
 
 def printReps():
-    print(string.ascii_uppercase)
+    print(alphabet)
     sorted_replacements = {v: k for k, v in replacements.items()}
-    for k,repl in sorted_replacements.items():
-        if repl != "|":
-            print(repl, end="")
+    for c in alphabet:
+        if c in sorted_replacements:
+            print(sorted_replacements[c], end="")
         else:
             print("-", end="")
 
@@ -55,15 +44,14 @@ while True:
         else:
             continue
     for i in range(len(keys)):
-        if keys[i] in string.ascii_uppercase:
-            if replacements[keys[i]] != values[i] and replacements[keys[i]] != "|":
+        if keys[i] in replacements:
+            if replacements[keys[i]] != values[i]:
                 print("CONFLICT FOUND with chatacter " + keys[i])
                 continue
             
-            replacements[keys[i]] = values[i]
+        replacements[keys[i]] = values[i]
         
     printReps()
     printText()
     print()
-    
     
